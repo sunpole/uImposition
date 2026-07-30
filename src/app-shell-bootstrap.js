bb57bedcd3e725112bca9b070e99ea31652b0d1d
ANTON MAGOMEDOV <akitsatnafiya@gmail.com>
2026-07-30 22:10:17 +0300

    UX-1: load narrow-phone shell overrides last

--- src/app-shell-bootstrap.js
+++ src/app-shell-bootstrap.js
@@ -1,7 +1,9 @@
-const stylesheet = document.createElement("link");
-stylesheet.rel = "stylesheet";
-stylesheet.href = "app-shell.css";
-stylesheet.setAttribute("data-app-shell-styles", "");
-document.head.append(stylesheet);
+for (const href of ["app-shell.css", "app-shell-overrides.css"]) {
+  const stylesheet = document.createElement("link");
+  stylesheet.rel = "stylesheet";
+  stylesheet.href = href;
+  stylesheet.setAttribute("data-app-shell-styles", href);
+  document.head.append(stylesheet);
+}
 
 await import("./app-shell.js");
